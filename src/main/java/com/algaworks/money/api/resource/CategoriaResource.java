e299cff13c2f8802433f7069c041a7e6772d835a
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-10 14:56:56 -0300

    Adicionando o recurso criar categoria

--- src/main/java/com/algaworks/money/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/money/api/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.algaworks.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.money.api.model.Categoria;
 import com.algaworks.money.api.repository.CategoriaRepository;
@@ -21,6 +30,17 @@ public class CategoriaResource {
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
 }
