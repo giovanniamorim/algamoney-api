93a347287969d0f5303722a2337f30312094ae6d
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-11 11:09:27 -0300

    Refatorando com EventPublisher e adicionando recursos a Pessoa

--- src/main/java/com/algaworks/money/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/money/api/resource/CategoriaResource.java
@@ -1,12 +1,12 @@
 package com.algaworks.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.money.api.event.RecursoCriadoEvent;
 import com.algaworks.money.api.model.Categoria;
 import com.algaworks.money.api.repository.CategoriaRepository;
 
@@ -26,6 +26,10 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	
 	@GetMapping
 	public List<Categoria> listar(){
@@ -37,11 +41,9 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/algaworks/money/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/money/api/resource/PessoaResource.java
@@ -2,11 +2,21 @@ package com.algaworks.money.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.money.api.event.RecursoCriadoEvent;
 import com.algaworks.money.api.model.Pessoa;
 import com.algaworks.money.api.repository.PessoaRepository;
 
@@ -17,10 +27,31 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
 		return pessoaRepository.findAll();
 	}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo){
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		if(pessoa == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.ok(pessoa);
+	}
+	
 	
 }
