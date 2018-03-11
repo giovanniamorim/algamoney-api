6b83b7c5cac6d0d72c0ab69319a707f058b9e572
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-11 11:35:48 -0300

    Adicionado o recurso DELETE em pessoas e tratamento de exceptions

--- src/main/java/com/algaworks/money/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/money/api/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.money.api.event.RecursoCriadoEvent;
@@ -51,6 +53,12 @@ public class PessoaResource {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return ResponseEntity.ok(pessoa);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 	
 	
