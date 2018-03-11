71a85ddbbc77dff3c9074d99d73bd8d9d489c33f
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-10 22:47:04 -0300

    Usando o BeanValidation para tratar erros nos campos

--- src/main/java/com/algaworks/money/api/model/Categoria.java
+++ src/main/java/com/algaworks/money/api/model/Categoria.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Categoria {
@@ -11,6 +13,9 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull(message = "O nome é obrigatório")
+	@Size(min=3, max=25, message = "O nome deve ter entre 3 e 25 caracteres")
 	private String nome;
 	
 	public Long getCodigo() {
