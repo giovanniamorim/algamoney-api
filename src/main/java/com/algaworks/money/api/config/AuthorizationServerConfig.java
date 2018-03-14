2c6a5728fd0b3ad5ce50efb5a8a97334f222c20f
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-14 15:50:33 -0300

    Adicionando permissões em nivel de scopo para aplicações consumirem serviços

--- src/main/java/com/algaworks/money/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/money/api/config/AuthorizationServerConfig.java
@@ -26,8 +26,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret("angular")
 			.scopes("read", "write")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(20)
-			.refreshTokenValiditySeconds(3600 * 24);
+			.accessTokenValiditySeconds(1800)
+			.refreshTokenValiditySeconds(3600 * 24)
+		.and()
+		.withClient("mobile")
+		.secret("mobile")
+		.scopes("read")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(1800)
+		.refreshTokenValiditySeconds(3600 * 24);
 	}
 	
 	@Override
---
+++ src/main/java/com/algaworks/money/api/repository/PermissaoRepository.java
@@ -0,0 +1,9 @@
+package com.algaworks.money.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.money.api.model.Permissao;
+
+public interface PermissaoRepository extends JpaRepository<Permissao, Long>{
+
+}
